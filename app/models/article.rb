31bb964cfbedcd773984cf86c557eb876d8f6441
jmayumikw <juliana.watanabe@enjoei.com.br>
2022-06-10 10:34:10 -0300

    Add one-to-many association between users an articles from back end

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    belongs_to :user
+
     # valida se o title está preenchido para poder salvar, e o tamanho mínimo e máximo
     validates :title, presence: true, length: { minimum: 6, maximum: 100}
     validates :description, presence: true, length: {minimum: 10, maximum: 1000}
