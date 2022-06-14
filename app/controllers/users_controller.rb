688ffd6c1ff6ebd300f19aa2b6387bf2c175d676
jmayumikw <juliana.watanabe@enjoei.com.br>
2022-06-14 09:49:29 -0300

    add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to Musheba App #{@user.username}, you have successfully signup!"
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
