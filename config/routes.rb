80231420258d4f823ee1cdc1865f8c28d94253f4
jmayumikw <juliana.watanabe@enjoei.com.br>
2022-06-14 09:28:13 -0300

    create user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :articles
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -10,5 +11,6 @@ Rails.application.routes.draw do
 
   get 'about', to: "pages#about"
 
-  resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
