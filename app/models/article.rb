467558d1e359a298c7cbbb8a3531da8278096d27
jmayumikw <juliana.watanabe@enjoei.com.br>
2022-07-18 16:13:19 -0300

    create migration article_categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord
-    belongs_to :user
-
-    # valida se o title está preenchido para poder salvar, e o tamanho mínimo e máximo
-    validates :title, presence: true, length: { minimum: 6, maximum: 100}
-    validates :description, presence: true, length: {minimum: 10, maximum: 1000}
+  belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  # valida se o title está preenchido para poder salvar, e o tamanho mínimo e máximo
+  validates :title, presence: true, length: { minimum: 6, maximum: 100}
+  validates :description, presence: true, length: {minimum: 10, maximum: 1000}
 end
